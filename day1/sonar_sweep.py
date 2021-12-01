a46c90f950003a1a7df90df92fd70784a1e8f1bc
Kristinus <bryan@draper.ai>
2021-12-01 16:45:51 -0500

    Day1

---
+++ day1/sonar_sweep.py
@@ -0,0 +1,30 @@
+import functools
+
+def part1(arr):
+    increase_count = 0
+    prev_depth = None
+    for depth in arr:
+        if prev_depth and depth > prev_depth:
+            increase_count += 1
+        prev_depth = depth
+    return increase_count
+
+
+def part2(arr):
+    increase_count = 0
+    w_len = 3
+    for i in range(w_len, len(arr)):
+        if arr[i] > arr[i-w_len]:
+            increase_count += 1
+    return increase_count
+
+
+if __name__ == "__main__":
+    f = open("input.txt", "r")
+    # f = open("sample.txt", "r")
+    
+    input = f.readlines()
+    arr = list(map(lambda x: int(x), input))
+    
+    print(part1(arr))
+    print(part2(arr))
