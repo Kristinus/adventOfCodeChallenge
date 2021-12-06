b6426f18370b282d56979dce71d850457937ebc2
Bryan Kristiono <bryan.m.kristiono@gmail.com>
2021-12-06 09:24:35 -0500

    day6

---
+++ day6/main.py
@@ -0,0 +1,14 @@
+import sys
+
+def part1(arr):
+    return
+
+def part2(arr):
+    return
+
+if __name__ == "__main__":
+    with open(sys.argv[1], "r") as f:
+        arr = f.read().splitlines()
+
+        print(part1(arr))
+        print(part2(arr))
